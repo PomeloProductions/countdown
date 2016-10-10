892cee33e80429ab69a4aeccfd625182874698d1
Bryce Meyer <bryce@axolotlinteractive.com>
2016-10-10 13:00:11 -0500

    removed admin config and moved short code registration to info.json

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -113,8 +113,6 @@ class Plugin extends LifeCycle {
 
         // Register short codes
         // http://plugin.michael-simpson.com/?page_id=39
-        $sc = new ShortCode($this);
-        $sc->register('countdown');
 
 
         // Register AJAX hooks
